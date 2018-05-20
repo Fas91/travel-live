c1b35c9733f72c8d6535fbe56e714dff2dece43a
Fas <fasf91x@gmail.com>
2018-05-19 21:33:23 -0400

    Completed header styles.

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,35 @@
+import MobileMenu from './modules/MobileMenu';
+
+
+
+var mobileMenu = new MobileMenu();
+
+
+
+
+
+
+
+
+
+
+// var $ = require('jquery');
+//
+// import Person from './modules/Person';
+//
+//
+// class Adult extends Person{
+//   payTaxes() {
+//     console.log(this.name + " pays taxes");
+//   }
+// }
+// alert(" 1234");
+//
+//
+// var john = new Person("John Doe", "blue");
+// john.greet();
+//
+// var jane = new Adult("Jane Smith", "blue");
+// jane.greet();
+// jane.payTaxes();
+//
